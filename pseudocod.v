ca4a0e2eae5979e70085273136a1db6255398beb
tifleadenisa <tifleadenisa@gmail.com>
2020-12-17 02:16:44 +0200

    Update pseudocod.v
    Schita pentru sintaxa

---
+++ pseudocod.v
@@ -0,0 +1,26 @@
+(*Schitarea ideilor*)
+
+(*memorie: o variabila poate fi nealocata sau alocata (caz in care are un offset-nr nat) *)
+
+(*o variabila (indiferent de tip) trebuie sa fie stocata la o adresa: nume_variabila->adresa*)
+
+(*fiecare adresa retine o valoare: adresa->valoare*)
+
+(* *)
+
+
+(*Urmeaza tipurile: *)
+(*AExp adaptat cu mem*)
+(*BExp adaptat cu mem*)
+(*Vector*)
+(*Stiva*)
+(*Coada*)
+(* ?Matrice? *)
+(* ?I/O? *)
+(* ?Tipuri de erori? *)
+(* Tipuri de valori *)
+
+(* Statements *)
+(* ?Environment? memorie + nume + tip + valoare*)
+(**)
+
